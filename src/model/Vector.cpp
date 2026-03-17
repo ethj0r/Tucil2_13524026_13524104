0976ca3238a952b2f254bb20d1baf862af960249
Daniel <19624107@mahasiswa.itb.ac.id>
2026-03-17 15:53:02 +0700

    feat: add struct vector 3D in model

---
+++ src/model/Vector.cpp
@@ -0,0 +1,34 @@
+#include "Vector.hpp"
+
+Vec plus(Vec a, Vec b){
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+Vec minus(Vec a, Vec b){
+    return {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+Vec scalarMul(Vec a, float b){
+    return {a.x * b, a.y * b, a.z * b};
+}
+
+float length(Vec a){
+    return sqrt(dot(a, a));
+}
+
+Vec normalize(Vec a){
+    float len = length(a);
+    if(len < 1e-6f){ // batas toleransi float
+        return {0.0f, 0.0f, 0.0f};
+    }
+
+    return scalarMul(a, 1.0f / len);
+}
+
+float dot(Vec a, Vec b){
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+Vec cross(Vec a, Vec b){
+    return {(a.y * b.z) - (b.y * a.z), (a.z * b.x) - (a.x * b.z), (a.x * b.y) - (a.y * b.x)};
+}
