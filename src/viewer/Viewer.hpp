4b79c9119350b17398ac4a0284464a1e14719f9d
Jordhy Branenda <ethgalleryin@gmail.com>
2026-03-20 18:01:55 +0700

    feat: viewer implementation v2

--- src/viewer/Viewer.hpp
+++ src/viewer/Viewer.hpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include "../model/BoundingBox.hpp"
 
+enum RenderMode { MODE_SOLID_WIRE, MODE_SOLID, MODE_WIRE, MODE_COUNT };
+
 /**
  * @brief launch 3D viewer window
- * @param voxels lst boundingbox dari leaf octree
+ * @param voxels list of AABB from octree leaf nodes
  */
 void launchViewer(const vector<AABB>& voxels);
